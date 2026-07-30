a8c86101d498d16435412532f722342635430f56
Nathan-McClorey <mcclornj@mail.uc.edu>
2026-07-30 19:30:09 -0400

    added checkout options

--- DBMS.py
+++ DBMS.py
@@ -2,8 +2,6 @@ import mysql.connector
 from tabulate import tabulate
 import os
 
-
-
 # Connect to MySQL server
 connection = mysql.connector.connect(
     host="localhost",
@@ -13,8 +11,6 @@ connection = mysql.connector.connect(
 cursor = connection.cursor()
 cursor.execute("USE libraryDB")
 
-
-# is this okay to keep or should i make this dictionary from our DB?
 criteria = {"1": "book_id",
         "2": "title",
         "3": "author",
@@ -23,28 +19,12 @@ criteria = {"1": "book_id",
 
 def browse_books(search_type = "all", search_id = "all", avail_type = "all", avail_id = "all"):
 
-# what to do if no books are found?
-
     # view all books
     if search_type == "all" and avail_type == "all":
         command = "SELECT * FROM books"
     # view specific books based on a search menu
     elif avail_type == "all":
         command = f"SELECT * FROM books WHERE {search_type} LIKE '%{search_id}%'"
-    # view available books at a specific library
-    elif search_type == "all":
-         command = f"SELECT b.* \
-                    FROM books AS b \
-                    JOIN available_books AS a\
-                    ON b.book_id = a.book_id\
-                    WHERE {avail_type} = '{avail_id}'"
-    else:
-        # not used yet, needed for checkout?
-        command = f"SELECT b.* AND a.city AND a.name \
-                    FROM books AS b \
-                    JOIN available_books AS a\
-                    ON b.book_id = a.book_id\
-                    WHERE b.{search_type} LIKE '%{search_id}% AND {avail_type} = '{avail_id}'"
         
     try:
         cursor.execute(command)
@@ -55,28 +35,11 @@ def browse_books(search_type = "all", search_id = "all", avail_type = "all", ava
 
             print(tabulate(results, headers=headers, tablefmt="grid"))
 
-        
-        #     results = cursor.fetchall()
-        #     for row in results:
-        #         print(f"""
-        #             # Book ID: {row[0]}
-        #             # Title: {row[1]}
-        #             # Author: {row[2]}
-        #             # Published: {row[3]}
-        #             # Genre: {row[4]}
-        #             # ------------------------
-        #             # """)
-
         else:
-            connection.commit()
-            print("Query executed successfully.")
+            print("no results found.")
     except Exception as e:
         print("Error:", e)
     
-    
-
-
-# missing AC 02.4 (you skipped 3?)
 def search_books():
 
     print("""
@@ -115,14 +78,9 @@ def search_books():
         ╚══════════════════════════════════════════╝
         """)
     print(f"Books where {criteria[user_choice]} contains '{search_by}':\n")
-    # print(f"Results for books with {criteria[user_choice]} including '{search_by}'")
     browse_books(criteria[user_choice], search_by)
     user_choice = input("Press Enter to continue...")
     os.system('cls')
-
-
-
-
 
 def view_book_availability():
     # Implementation for viewing book availability
@@ -216,6 +174,8 @@ def view_book_availability():
             except Exception as e:
                 print("Error:", e)
 
+        case _:
+            print("Invalid option.")
 
 def checkout():
     print("To checkout a book, please enter the following information:")
@@ -226,31 +186,17 @@ def checkout():
 
     try:
         cursor.execute("SELECT title FROM books WHERE book_id = %s", (book_id,))
-        
-        s = input("Press e...")
         book_name = cursor.fetchone()
         
-        s = input("Press e...")
         cursor.execute("SELECT city, name FROM libraries WHERE library_id = %s", (library_id,))
-        
-        s = input("Press e...")
         library_info = cursor.fetchone()
-        
-        s = input("Press e...")
 
         print(f"\n\nchecking out '{book_name}' from '{library_info[0]}, {library_info[1]}':\n")
-
-        s = input("Press e...")
-        
-
         cursor.execute("""
             DELETE FROM availability
             WHERE library_id = %s
             AND book_id = %s
         """, (library_id, book_id))
-
-        s = input("Press e...")
-
 
         if cursor.rowcount == 0:
             s = input("Your book does not exist in the specified library.")
@@ -277,9 +223,24 @@ def checkout():
     except Exception as e:
         
         print("Error:", e)
-        s = input("Press Enter fdfdfdfdfto asdfasdfsafcontinue...")
+        s = input("Press Enter to continue...")
+
+def current_checkout():
 
 
+
+    try:
+        cursor.execute("SELECT * FROM checkouts")
+        if cursor.description:
+            results = cursor.fetchall()
+            headers = [column[0] for column in cursor.description]
+            print(tabulate(results, headers=headers, tablefmt="grid"))
+
+        else:
+            print("No current checkouts.")
+
+    except Exception as e:
+        print("Error:", e)
 
 # -----------------------------------------------
 # -----------------------------------------------
@@ -296,11 +257,6 @@ print("\tWelcome to the Fake Library Association")
 print("-------------------------------------------------------\n")
 
 while True:
-    # print("Please Choose an Option Below (ex.1,2,3,4)\n")
-    # print("1.) Browse All Books")
-    # print("2.) Search for a Book")
-    # print("3.) View Book Location Availability")
-    # print("4.) Exit\n")
 
     print("""
 ╔══════════════════════════════════════╗
@@ -311,12 +267,13 @@ while True:
 ║  2.) Search for a Book               ║
 ║  3.) View Book Location Availability ║
 ║  4.) Check Out a Book                ║
+║  5.) View Current Checkouts          ║
 ║                                      ║
-║  5.) Exit                            ║
+║  6.) Exit                            ║
 ╚══════════════════════════════════════╝
 """)
 
-    user_choice = input("Select an option (1-5): ")
+    user_choice = input("Select an option (1-6): ")
     
     os.system('cls')
     match user_choice :
@@ -330,12 +287,15 @@ while True:
             user_choice = input("Press Enter to continue...")
         case "4":
             checkout()
+            
         case "5":
+            current_checkout()
+            user_choice = input("Press Enter to continue...")
+        case "6":
             break
         case _:
             print("Invalid Option")
             user_choice = input("Press Enter to continue...")
-
     os.system('cls')
 
 cursor.close()
